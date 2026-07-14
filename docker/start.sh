ed5d56eceedb3f4e768d5abe526a4749ef6a1e91
Kamleshnishad <paabclinic@gmail.com>
2026-07-14 13:24:43 +0530

    Add Docker + CI/CD deployment pipeline

---
+++ docker/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+cd /var/www/html
+
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan migrate --force
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+exec /usr/bin/supervisord -c /etc/supervisord.conf
